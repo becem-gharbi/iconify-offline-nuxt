2eec8487c494aa7bbb08905ab05524cbb5890c5d
becem-gharbi <becem.gharbi@live.com>
2024-01-29 10:18:45 +0100

    chore: add a static prerendered page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,7 @@ export default defineNuxtConfig({
       },
     },
   },
+  routeRules: {
+    '/static': { prerender: true }
+  }
 });
---
+++ pages/static.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Icons</h3>
+    <Iconify name="heroicons:arrow-down-left-16-solid" />
+    <Iconify name="iconamoon:arrow-right-6-circle-duotone" />
+    <Iconify :name="icon1" />
+    <Iconify :name="icon2" />
+    <Iconify :name="icon3" />
+  </div>
+</template>
+
+<script setup>
+const icon1 = "majesticons:bookmark-plus-line"
+const icon2 = 'majesticons:airplane-line'
+const icon3 = `flowbite:adjustments-vertical-outline`
+</script>
