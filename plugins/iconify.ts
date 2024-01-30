35f95a72e70d0c9b32c64f661c11ae4a5fe12242
becem-gharbi <becem.gharbi@live.com>
2024-01-30 15:41:54 +0100

    refactor: avoid dynamic import with variables

--- plugins/iconify.ts
+++ plugins/iconify.ts
@@ -5,13 +5,17 @@ import { defu } from 'defu'
 export default defineNuxtPlugin(() => {
   if (process.dev) { return }
 
+  const imports = process.server
+    ? import.meta.glob('~/public/iconify/*/*.json', { import: 'default' })
+    : {}
+
   _api.setFetch(async (req) => {
     const url = req.toString()
     const prefix = parseURL(url).pathname.split('/').pop()!.replace('.json', '')
     const icons = getQuery<{ icons: string }>(url).icons.split(',')
 
     const iconsData = process.server
-      ? await Promise.all(icons.map(i => import(`~/public/iconify/${prefix}/${i}.json`).then(m => m.default)))
+      ? await Promise.all<any>(icons.map(i => imports[`/public/iconify/${prefix}/${i}.json`]()))
       : await Promise.all(icons.map(i => $fetch(`/iconify/${prefix}/${i}.json`)))
 
     const iconsDataMerged = defu({}, ...iconsData)
