217e817a14472006b45a0493be49ca1793afae41
becem-gharbi <becem.gharbi@live.com>
2024-01-30 11:44:01 +0100

    chore: add client-only icons

---
+++ pages/client.vue
@@ -0,0 +1,7 @@
+<template>
+  <client-only>
+    <Iconify name="carbon:3d-mpr-toggle" />
+    <Iconify name="basil:dribbble-outline" />
+    <Iconify name="logos:vue" />
+  </client-only>
+</template>
