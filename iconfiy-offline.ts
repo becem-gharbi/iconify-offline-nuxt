3f52929b0e0d5f3c16bdd796f6331b35691e6c56
becem-gharbi <becem.gharbi@live.com>
2024-01-30 11:28:52 +0100

    refactor: extract utilties from vite plugin

--- iconfiy-offline.ts
+++ iconfiy-offline.ts
@@ -5,6 +5,49 @@ import { loadIcons, getIcon } from '@iconify/vue'
 import type { IconifyJSON, IconifyIcon, IconifyIconName } from '@iconify/vue'
 import type { Plugin } from 'vite'
 
+const download = (icons: string[]) => new Promise<IconifyJSON[]>((resolve, reject) => {
+  loadIcons(icons, (loaded, missing, pending) => {
+    if (pending.length) { return }
+
+    const fullname = (e: IconifyIconName) => `${e.prefix}:${e.name}`
+
+    if (missing.length) {
+      const error = new Error(`❌ 󠀠 [iconify-download-icons] download failed of ${missing.map(fullname).join(' ')}`)
+      reject(error)
+    }
+
+    const output = loaded.map(i => ({
+      prefix: i.prefix,
+      icons: {
+        [i.name]: getIcon(fullname(i)) as IconifyIcon
+      }
+    }))
+
+    resolve(output)
+  })
+})
+
+const makeDir = (path: string, replace = false) => {
+  const exists = fs.existsSync(path)
+  if (exists && replace) {
+    fs.rmSync(path, { recursive: true, force: true })
+  }
+  if (!exists || replace) {
+    fs.mkdirSync(path)
+  }
+}
+
+const save = (iconsJSON: IconifyJSON[], iconsDir: string) => {
+  iconsJSON.forEach((i) => {
+    const prefix = i.prefix
+    const name = Object.keys(i.icons)[0]
+    const dirPath = path.resolve(iconsDir, prefix)
+    const filePath = path.resolve(dirPath, `${name}.json`)
+    makeDir(dirPath)
+    fs.writeFileSync(filePath, JSON.stringify(i))
+  })
+}
+
 export default function (rootDir = './'): Plugin | undefined {
   if (process.env.NODE_ENV === 'development') { return }
 
@@ -20,63 +63,20 @@ export default function (rootDir = './'): Plugin | undefined {
 
   const icons = new Set<string>()
 
+  const iconsDir = path.resolve(rootDir, 'public', 'iconify')
+
   return {
     name: 'iconify-download-icons',
 
     transform (code) {
-      const matches = code.match(regex)
-      matches?.forEach(m => icons.add(m.replace(/'|"|`/g, '')))
+      code.match(regex)?.forEach(m => icons.add(m.replace(/'|"|`/g, '')))
       return { code, map: null }
     },
 
     async buildEnd () {
-      const iconsDir = path.resolve(rootDir, 'public', 'iconify')
-
-      const makeDir = (path: string, replace = false) => {
-        const exists = fs.existsSync(path)
-        if (exists && replace) {
-          fs.rmSync(path, { recursive: true, force: true })
-        }
-        if (!exists || replace) {
-          fs.mkdirSync(path)
-        }
-      }
-
-      const save = (iconsJSON: IconifyJSON[]) => {
-        iconsJSON.forEach((i) => {
-          const prefix = i.prefix
-          const name = Object.keys(i.icons)[0]
-          const dirPath = path.resolve(iconsDir, prefix)
-          const filePath = path.resolve(dirPath, `${name}.json`)
-          makeDir(dirPath)
-          fs.writeFileSync(filePath, JSON.stringify(i))
-        })
-      }
-
-      const download = (icons: string[]) => new Promise<IconifyJSON[]>((resolve, reject) => {
-        loadIcons(icons, (loaded, missing, pending) => {
-          if (pending.length) { return }
-
-          const fullname = (e: IconifyIconName) => `${e.prefix}:${e.name}`
-
-          if (missing.length) {
-            const error = new Error(`❌ 󠀠 [iconify-download-icons] download failed of ${missing.map(fullname).join(' ')}`)
-            reject(error)
-          }
-
-          const output = loaded.map(i => ({
-            prefix: i.prefix,
-            icons: {
-              [i.name]: getIcon(fullname(i)) as IconifyIcon
-            }
-          }))
-
-          resolve(output)
-        })
-      })
-
       makeDir(iconsDir, true)
-      await download([...icons.values()]).then(save)
+
+      await download([...icons.values()]).then(d => save(d, iconsDir))
 
       /* eslint-disable no-console */
       console.log(`✔️ 󠀠 [iconify-download-icons] saved ${icons.size} icons to ${iconsDir}`)
