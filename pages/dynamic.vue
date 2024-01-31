4377a0eadb40a77da12e05086c11a6fe6e5c2e7c
becem-gharbi <becem.gharbi@live.com>
2024-01-31 09:59:51 +0100

    chore: add dynamic Icons

---
+++ pages/dynamic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Iconify :name="icon" />
+    <button @click="swap">
+      swap
+    </button>
+  </div>
+</template>
+
+<script setup>
+const icon1 = 'carbon:bat'
+const icon2 = 'carbon:bare-metal-server'
+const icon = ref(icon1)
+const swap = () => {
+  icon.value = icon.value === icon1 ? icon2 : icon1
+}
+</script>
